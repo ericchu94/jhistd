3689edca6f066f08a5800636a82d4a86dd02dbbd
Eric Chu <ericchu@yelp.com>
2016-11-11 11:55:48 -0800

    Begin adding socket.io

--- jhistd.js
+++ jhistd.js
@@ -6,61 +6,86 @@ const timestamp = require('monotonic-timestamp');
 const cors = require('koa-cors');
 const convert = require('koa-convert');
 const bodyParser = require('koa-bodyparser');
+const IO = require('koa-socket');
 
 const Player = require('./models/player');
 
 const app = new Koa();
+const io = new IO();
 const api1 = new Router({
   prefix: '/api/v1',
 });
+
+async function getPlayers() {
+  return await Player.findAll();
+}
+
+async function postPlayer(name) {
+  return await Player.create({
+    name: name,
+  });
+}
+
+async function getPlayer(id) {
+  return await Player.findById(id);
+}
+
+async function putPlayer(id, name, score) {
+  const values = {};
+  if (name)
+    values.name = name;
+  if (score)
+    values.score = score;
+  const [affectedCount] = await Player.update(values, {
+    where: {
+      id: id,
+    },
+  });
+  return affectedCount || null;
+}
+
+async function deletePlayer(id) {
+  const [affectedCount] = await Player.drop({
+    where: {
+      id: id,
+    },
+  });
+  return affectedCount || null;
+}
 
 api1.get('/swagger.(json|yaml)', async function (ctx) {
   await send(ctx, ctx.path);
 });
 
 api1.get('/players', async function (ctx) {
-  const players = await Player.findAll({
-  });
-  ctx.body = players;
+  ctx.body = await getPlayers();
 });
 
 api1.post('/players', bodyParser(), async function (ctx) {
   const {name} = ctx.request.body;
-  const player = await Player.create({
-    name: name,
-  });
-  ctx.body = player;
+  ctx.body = await postPlayer(name);
 });
 
 api1.get('/players/:id', async function (ctx) {
-  const player = await Player.findById(ctx.params.id, {
-  });
-  if (player)
-    ctx.body = player;
+  const id = ctx.params.id;
+  const body = await getPlayer(id);
+  if (body)
+    ctx.body = body;
 });
 
 api1.put('/players/:id', bodyParser(), async function (ctx) {
+  const id = ctx.params.id;
   const {name, score} = ctx.request.body;
-  const [affectedCount] = await Player.update({
-    name: name,
-    score: score,
-  }, {
-    where: {
-      id: ctx.params.id,
-    },
-  });
-  if (affectedCount > 0)
-    ctx.status = 200;
+  const body = await putPlayer(id, name, score);
+  if (body)
+    ctx.body = body;
 });
 
 api1.delete('/players/:id', async function (ctx) {
-  const [affectedCount] = await Player.drop({
-    where: {
-      id: ctx.params.id,
-    },
-  });
-  if (affectedCount > 0)
-    ctx.status = 200;
+  const id = ctx.params.id;
+  const body = await deletePlayer(id);
+  if (body)
+    ctx.body = body;
 });
 
 app.use(convert(cors({
@@ -77,4 +102,5 @@ app.use(async function (ctx, next) {
 
 app.use(api1.routes(), api1.allowedMethods());
 
+io.attach(app);
 app.listen(process.env.PORT || 3000);
